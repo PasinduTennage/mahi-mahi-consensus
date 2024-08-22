d8b9126525fe22cf6306b1a6c2ac323c3d01801d
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-08-22 14:31:52 +0200

    added aws ip.sh

---
+++ experiments/aws/ip.sh
@@ -0,0 +1,34 @@
+cert="/home/pasindu/Pictures/pasindu"
+username=ubuntu
+replica1_name=18.222.145.197
+replica1=${username}@${replica1_name}
+
+replica2_name=3.144.146.147
+replica2=${username}@${replica2_name}
+
+replica3_name=54.218.252.47
+replica3=${username}@${replica3_name}
+
+replica4_name=35.88.241.221
+replica4=${username}@${replica4_name}
+
+replica5_name=13.247.111.153
+replica5=${username}@${replica5_name}
+
+replica6_name=13.246.2.134
+replica6=${username}@${replica6_name}
+
+replica7_name=18.162.46.231
+replica7=${username}@${replica7_name}
+
+replica8_name=18.163.196.57
+replica8=${username}@${replica8_name}
+
+replica9_name=15.161.238.67
+replica9=${username}@${replica9_name}
+
+replica10_name=35.152.97.106
+replica10=${username}@${replica10_name}
+
+replicas=(${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${replica6} ${replica7} ${replica8} ${replica9} ${replica10})
+replica_names=(${replica1_name} ${replica2_name} ${replica3_name} ${replica4_name} ${replica5_name} ${replica6_name} ${replica7_name} ${replica8_name} ${replica9_name} ${replica10_name})
