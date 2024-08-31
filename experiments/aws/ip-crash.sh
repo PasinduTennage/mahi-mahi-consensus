05a8ad07c4de7793a47375f971c1f1bd1956d7e3
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-09-01 00:42:03 +0200

    added and checked ip-crash.sh

---
+++ experiments/aws/ip-crash.sh
@@ -0,0 +1,34 @@
+cert="/home/ubuntu/Pictures/pasindu"
+username=ubuntu
+replica1_name=18.118.137.201
+replica1=${username}@${replica1_name}
+
+replica2_name=3.144.123.71
+replica2=${username}@${replica2_name}
+
+replica3_name=52.38.189.236
+replica3=${username}@${replica3_name}
+
+replica4_name=35.87.106.181
+replica4=${username}@${replica4_name}
+
+replica5_name=13.246.137.53
+replica5=${username}@${replica5_name}
+
+replica6_name=13.247.106.235
+replica6=${username}@${replica6_name}
+
+replica7_name=43.198.251.218
+replica7=${username}@${replica7_name}
+
+replica8_name=43.198.205.87
+replica8=${username}@${replica8_name}
+
+replica9_name=15.160.27.166
+replica9=${username}@${replica9_name}
+
+replica10_name=15.161.235.103
+replica10=${username}@${replica10_name}
+
+replicas=(${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${replica6} ${replica7} ${replica8} ${replica9} ${replica10})
+replica_names=(${replica1_name} ${replica2_name} ${replica3_name} ${replica4_name} ${replica5_name} ${replica6_name} ${replica7_name} ${replica8_name} ${replica9_name} ${replica10_name})
