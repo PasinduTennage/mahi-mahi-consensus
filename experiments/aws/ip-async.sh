cb7d7fbab23a42c47a17a981d16761f657de4191
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-09-01 15:01:08 +0200

    added and checked ip-async.sh

---
+++ experiments/aws/ip-async.sh
@@ -0,0 +1,34 @@
+cert="/home/ubuntu/Pictures/pasindu"
+username=ubuntu
+replica1_name=13.59.233.152
+replica1=${username}@${replica1_name}
+
+replica2_name=3.145.64.235
+replica2=${username}@${replica2_name}
+
+replica3_name=44.243.127.150
+replica3=${username}@${replica3_name}
+
+replica4_name=52.36.125.236
+replica4=${username}@${replica4_name}
+
+replica5_name=13.244.216.25
+replica5=${username}@${replica5_name}
+
+replica6_name=13.246.188.140
+replica6=${username}@${replica6_name}
+
+replica7_name=43.198.101.139
+replica7=${username}@${replica7_name}
+
+replica8_name=18.162.228.57
+replica8=${username}@${replica8_name}
+
+replica9_name=15.160.238.213
+replica9=${username}@${replica9_name}
+
+replica10_name=15.160.204.133
+replica10=${username}@${replica10_name}
+
+replicas=(${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${replica6} ${replica7} ${replica8} ${replica9} ${replica10})
+replica_names=(${replica1_name} ${replica2_name} ${replica3_name} ${replica4_name} ${replica5_name} ${replica6_name} ${replica7_name} ${replica8_name} ${replica9_name} ${replica10_name})
