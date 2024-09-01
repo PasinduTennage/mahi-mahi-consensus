7ddde23c456b321430e12442b67bab4107951aba
“Pasindu <pasindunivanthakatennage@gmail.com>
2024-09-02 01:48:39 +0200

    added dry_run; all files checked

--- scripts/dryrun.sh
+++ scripts/dryrun.sh
@@ -2,19 +2,30 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # bash run.sh
+output_dir=logs/local/
+mkdir -p ${output_dir}
+
+pkill -f mysticeti
 
 cargo build
 
 export RUST_LOG=warn,mysticeti_core::consensus=debug,mysticeti_core::net_sync=warn,mysticeti_core::core=debug
 
-nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 0 > v0.log.ansi &
-nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 1 > v1.log.ansi &
-nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 2 > v2.log.ansi &
-nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 3 > v3.log.ansi &
 
-sleep 60
-pkill -f mysticeti
+nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 0 > ${output_dir}v0.log.ansi &
+nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 1 > ${output_dir}v1.log.ansi &
+nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 2 > ${output_dir}v2.log.ansi &
+nohup ./target/debug/mysticeti  dry-run --committee-size 4 --authority 3 > ${output_dir}v3.log.ansi &
 
-python3 scripts/block-create-test.py v0.log.ansi v1.log.ansi v2.log.ansi v3.log.ansi
-python3 scripts/commit-test.py v0.log.ansi v1.log.ansi v2.log.ansi v3.log.ansi
-python3 scripts/simple-commit-count.py v0.log.ansi v1.log.ansi v2.log.ansi v3.log.ansi
+sleep 130
+
+python3 scripts/block-create-test.py   ${output_dir}v0.log.ansi ${output_dir}v1.log.ansi ${output_dir}v2.log.ansi ${output_dir}v3.log.ansi
+python3 scripts/commit-test.py         ${output_dir}v0.log.ansi ${output_dir}v1.log.ansi ${output_dir}v2.log.ansi ${output_dir}v3.log.ansi
+python3 scripts/simple-commit-count.py ${output_dir}v0.log.ansi ${output_dir}v1.log.ansi ${output_dir}v2.log.ansi ${output_dir}v3.log.ansi
+
+output_file=${output_dir}stats.log
+
+python3 experiments/python/client-stats.py client-times-0.txt 5 0 >> ${output_file}
+python3 experiments/python/client-stats.py client-times-1.txt 5 1 >> ${output_file}
+python3 experiments/python/client-stats.py client-times-2.txt 5 2 >> ${output_file}
+python3 experiments/python/client-stats.py client-times-3.txt 5 3 >> ${output_file}
