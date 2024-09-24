925bdfdf69b1efe7f9990a76418a2e1073242eab
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-24 15:02:12 +0200

    checked ip-50

--- experiments/aws/ip-best-50.sh
+++ experiments/aws/ip-best-50.sh
@@ -1,154 +1,154 @@
 cert="/home/ubuntu/Pictures/pasindu"
 username=ubuntu
 
-replica1_name=18.116.23.255
+replica1_name=18.220.32.12
 replica1=${username}@${replica1_name}
 
-replica2_name=3.139.107.162
+replica2_name=18.222.46.243
 replica2=${username}@${replica2_name}
 
-replica3_name=18.225.254.160
+replica3_name=18.221.172.99
 replica3=${username}@${replica3_name}
 
-replica4_name=3.135.197.64
+replica4_name=3.22.248.108
 replica4=${username}@${replica4_name}
 
-replica5_name=3.135.186.218
+replica5_name=3.144.82.234
 replica5=${username}@${replica5_name}
 
-replica6_name=52.14.185.16
+replica6_name=3.144.28.54
 replica6=${username}@${replica6_name}
 
-replica7_name=18.189.170.166
+replica7_name=3.144.110.219
 replica7=${username}@${replica7_name}
 
-replica8_name=13.59.175.64
+replica8_name=3.15.182.75
 replica8=${username}@${replica8_name}
 
-replica9_name=3.129.247.234
+replica9_name=18.191.210.147
 replica9=${username}@${replica9_name}
 
-replica10_name=52.14.164.37
+replica10_name=18.191.236.140
 replica10=${username}@${replica10_name}
 
-replica11_name=35.94.18.120
+replica11_name=35.90.128.170
 replica11=${username}@${replica11_name}
 
-replica12_name=54.189.153.10
+replica12_name=54.191.34.150
 replica12=${username}@${replica12_name}
 
-replica13_name=35.90.17.189
+replica13_name=35.167.66.241
 replica13=${username}@${replica13_name}
 
-replica14_name=34.220.116.152
+replica14_name=54.148.8.11
 replica14=${username}@${replica14_name}
 
-replica15_name=54.68.56.50
+replica15_name=54.189.40.196
 replica15=${username}@${replica15_name}
 
-replica16_name=34.215.22.71
+replica16_name=35.88.87.236
 replica16=${username}@${replica16_name}
 
-replica17_name=18.236.187.93
+replica17_name=44.243.72.130
 replica17=${username}@${replica17_name}
 
-replica18_name=35.94.45.58
+replica18_name=18.237.232.83
 replica18=${username}@${replica18_name}
 
-replica19_name=54.202.120.83
+replica19_name=34.219.85.73
 replica19=${username}@${replica19_name}
 
-replica20_name=52.39.20.212
+replica20_name=54.186.240.207
 replica20=${username}@${replica20_name}
 
-replica21_name=13.245.237.152
+replica21_name=13.245.224.57
 replica21=${username}@${replica21_name}
 
-replica22_name=13.244.100.28
+replica22_name=13.247.114.202
 replica22=${username}@${replica22_name}
 
-replica23_name=13.245.38.97
+replica23_name=13.244.239.159
 replica23=${username}@${replica23_name}
 
-replica24_name=13.245.15.245
+replica24_name=13.244.75.67
 replica24=${username}@${replica24_name}
 
-replica25_name=13.247.87.80
+replica25_name=13.245.224.237
 replica25=${username}@${replica25_name}
 
-replica26_name=13.245.232.177
+replica26_name=13.245.16.188
 replica26=${username}@${replica26_name}
 
-replica27_name=13.247.113.20
+replica27_name=13.246.26.150
 replica27=${username}@${replica27_name}
 
-replica28_name=13.245.17.148
+replica28_name=13.245.35.88
 replica28=${username}@${replica28_name}
 
-replica29_name=13.246.193.126
+replica29_name=13.246.58.103
 replica29=${username}@${replica29_name}
 
-replica30_name=13.244.232.173
+replica30_name=13.245.141.185
 replica30=${username}@${replica30_name}
 
-replica31_name=43.198.98.49
+replica31_name=43.198.223.213
 replica31=${username}@${replica31_name}
 
-replica32_name=16.163.0.189
+replica32_name=18.162.39.192
 replica32=${username}@${replica32_name}
 
-replica33_name=18.162.109.98
+replica33_name=43.198.204.56
 replica33=${username}@${replica33_name}
 
-replica34_name=43.198.182.82
+replica34_name=18.163.3.183
 replica34=${username}@${replica34_name}
 
-replica35_name=18.167.173.237
+replica35_name=18.163.208.2
 replica35=${username}@${replica35_name}
 
-replica36_name=43.198.186.62
+replica36_name=18.162.194.247
 replica36=${username}@${replica36_name}
 
-replica37_name=18.166.210.203
+replica37_name=43.198.151.99
 replica37=${username}@${replica37_name}
 
-replica38_name=18.163.210.191
+replica38_name=18.167.167.21
 replica38=${username}@${replica38_name}
 
-replica39_name=18.167.54.180
+replica39_name=16.163.123.19
 replica39=${username}@${replica39_name}
 
-replica40_name=18.163.122.8
+replica40_name=18.167.69.176
 replica40=${username}@${replica40_name}
 
-replica41_name=35.152.135.147
+replica41_name=15.161.150.205
 replica41=${username}@${replica41_name}
 
-replica42_name=15.161.234.217
+replica42_name=18.102.213.120
 replica42=${username}@${replica42_name}
 
-replica43_name=15.161.48.195
+replica43_name=15.160.248.135
 replica43=${username}@${replica43_name}
 
-replica44_name=15.160.40.233
+replica44_name=15.160.246.169
 replica44=${username}@${replica44_name}
 
-replica45_name=35.152.68.34
+replica45_name=18.102.60.178
 replica45=${username}@${replica45_name}
 
-replica46_name=15.160.29.189
+replica46_name=15.160.60.126
 replica46=${username}@${replica46_name}
 
-replica47_name=35.152.48.212
+replica47_name=15.161.126.36
 replica47=${username}@${replica47_name}
 
-replica48_name=15.160.237.229
+replica48_name=15.161.197.155
 replica48=${username}@${replica48_name}
 
-replica49_name=35.152.69.75
+replica49_name=15.161.149.234
 replica49=${username}@${replica49_name}
 
-replica50_name=15.161.50.29
+replica50_name=15.160.160.188
 replica50=${username}@${replica50_name}
 
 replicas=(${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${replica6} ${replica7} ${replica8} ${replica9} ${replica10} ${replica11} ${replica12} ${replica13} ${replica14} ${replica15} ${replica16} ${replica17} ${replica18} ${replica19} ${replica20} ${replica21} ${replica22} ${replica23} ${replica24} ${replica25} ${replica26} ${replica27} ${replica28} ${replica29} ${replica30} ${replica31} ${replica32} ${replica33} ${replica34} ${replica35} ${replica36} ${replica37} ${replica38} ${replica39} ${replica40} ${replica41} ${replica42} ${replica43} ${replica44} ${replica45} ${replica46} ${replica47} ${replica48} ${replica49} ${replica50})
