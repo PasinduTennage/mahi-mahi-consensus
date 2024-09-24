0ab61475ed46d983dd131b2f6c17048fc9dd2ca9
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-24 18:02:08 +0200

    ip best 10 checked

--- experiments/aws/ip-best.sh
+++ experiments/aws/ip-best.sh
@@ -1,33 +1,33 @@
 cert="/home/ubuntu/Pictures/pasindu"
 username=ubuntu
-replica1_name=3.148.117.186
+replica1_name=3.145.165.189
 replica1=${username}@${replica1_name}
 
-replica2_name=18.221.177.227
+replica2_name=3.144.244.20
 replica2=${username}@${replica2_name}
 
-replica3_name=34.220.187.3
+replica3_name=35.164.6.4
 replica3=${username}@${replica3_name}
 
-replica4_name=54.70.139.32
+replica4_name=34.211.30.86
 replica4=${username}@${replica4_name}
 
-replica5_name=13.247.110.80
+replica5_name=13.246.189.89
 replica5=${username}@${replica5_name}
 
-replica6_name=13.244.102.184
+replica6_name=13.244.115.213
 replica6=${username}@${replica6_name}
 
-replica7_name=43.198.215.67
+replica7_name=16.162.4.19
 replica7=${username}@${replica7_name}
 
-replica8_name=43.198.106.124
+replica8_name=43.198.205.206
 replica8=${username}@${replica8_name}
 
-replica9_name=15.160.48.23
+replica9_name=15.160.137.17
 replica9=${username}@${replica9_name}
 
-replica10_name=15.161.185.219
+replica10_name=35.152.61.164
 replica10=${username}@${replica10_name}
 
 replicas=(${replica1} ${replica2} ${replica3} ${replica4} ${replica5} ${replica6} ${replica7} ${replica8} ${replica9} ${replica10})
