7c928c06bd436c96b94065119e0e8c2d4ac1bcb6
Pasindu  Tennage <pasindunivanthakatennage@gmail.com>
2025-07-20 22:30:42 +0100

    Update config.rs

--- crates/mysticeti-core/src/config.rs
+++ crates/mysticeti-core/src/config.rs
@@ -64,7 +64,7 @@ pub mod node_defaults {
     }
 
     pub fn default_max_block_size() -> usize {
-        8 * 1024 * 1024
+        4 * 1024 * 1024
     }
 
     pub fn default_rounds_in_epoch() -> super::RoundNumber {
